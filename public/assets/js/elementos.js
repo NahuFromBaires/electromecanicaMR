504f3a69ec4ffdcdb68ea10b93464e005d096308
Nahu <nahuargentina@gmail.com>
2021-09-28 13:47:49 -0300

    POSTA: Menu hamburguer se oculta en Mobile al click

--- public/assets/js/elementos.js
+++ public/assets/js/elementos.js
@@ -5,8 +5,7 @@ document.addEventListener("DOMContentLoaded", function(){
     for(var x=0; x<links.length; x++){
        links[x].onclick = function(){
           document.querySelector("button.navbar-toggler").click();
-          console.log("Pase por aca");
-          alert("Holis")
+
        }
     }
  });
