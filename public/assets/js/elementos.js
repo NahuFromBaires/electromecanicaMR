20d1e8d6c057b5c0ee7b4ca5ff4f3edb270e04a5
Nahu <nahuargentina@gmail.com>
2021-09-28 13:32:20 -0300

    Arreglos en menu hamburguer

---
+++ public/assets/js/elementos.js
@@ -0,0 +1,10 @@
+
+//Oculta el hamburguer menu, al tocar sobre un elemento en vista Mobile
+document.addEventListener("DOMContentLoaded", function(){
+    var links = document.querySelectorAll(".navbar-nav li a:not([href='#'])");
+    for(var x=0; x<links.length; x++){
+       links[x].onclick = function(){
+          document.querySelector("button.navbar-toggler").click();
+       }
+    }
+ });
